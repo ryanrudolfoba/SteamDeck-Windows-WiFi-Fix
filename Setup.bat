da9d60c4f146d2429dce7ab0e253e67fde260622
ryanrudolfoba <98122529+ryanrudolfoba@users.noreply.github.com>
2023-08-22 09:12:36 -0400

    Added logic and used InterfaceDescription

--- Setup.bat
+++ Setup.bat
@@ -20,34 +20,42 @@ echo Done!
 echo Installing Realtek Killer Driver...
 pnputil /add-driver "Driver\netrtwlane.inf" /install
 echo.
-echo pausing for 10secs...
-ping -n 10 localhost > nul
+echo pausing for 5secs...
+ping -n 5 localhost > nul
 echo.
+echo Checking if the unlocked driver is installed.
+powershell get-netadapter | findstr /c:"RTK Killer Wi-Fi 5 8822CE Xtreme"
+if ERRORLEVEL 1 goto :driver_error
+echo.
+echo Unlocked driver is successfully installed!
 echo Applying Steam Deck Advanced WiFi Driver Settings by ryanrudolf
-echo Set-NetAdapterAdvancedProperty -Name "Wi-Fi" -DisplayName "11n Adhoc" -DisplayValue "Disabled" > settings.ps1
-echo Set-NetAdapterAdvancedProperty -Name "Wi-Fi" -DisplayName "802.11d" -DisplayValue "Disabled" >> settings.ps1
-echo Set-NetAdapterAdvancedProperty -Name "Wi-Fi" -DisplayName "802.11n channel width for 2.4GHz" -DisplayValue "Auto" >> settings.ps1
-echo Set-NetAdapterAdvancedProperty -Name "Wi-Fi" -DisplayName "802.11n channel width for 5.2GHz" -DisplayValue "Auto" >> settings.ps1
-echo Set-NetAdapterAdvancedProperty -Name "Wi-Fi" -DisplayName "Bandwidth" -DisplayValue "80+40+20Mhz" >> settings.ps1
-echo Set-NetAdapterAdvancedProperty -Name "Wi-Fi" -DisplayName "Beacon Interval" -DisplayValue "100" >> settings.ps1
-echo Set-NetAdapterAdvancedProperty -Name "Wi-Fi" -DisplayName "Classroom mode" -DisplayValue "Enabled" >> settings.ps1
-echo Set-NetAdapterAdvancedProperty -Name "Wi-Fi" -DisplayName "Concurrent Operation Preference"-DisplayValue "No Preference" >> settings.ps1
-echo Set-NetAdapterAdvancedProperty -Name "Wi-Fi" -DisplayName "HT mode" -DisplayValue "VHT mode" >> settings.ps1
-echo Set-NetAdapterAdvancedProperty -Name "Wi-Fi" -DisplayName "MAC Randomization" -DisplayValue "Enabled" >> settings.ps1
-echo Set-NetAdapterAdvancedProperty -Name "Wi-Fi" -DisplayName "Mixed mode protection" -DisplayValue "CTS-to-self Enabled" >> settings.ps1
-echo Set-NetAdapterAdvancedProperty -Name "Wi-Fi" -DisplayName "Multi-Channel Concurrent" -DisplayValue "Enabled" >> settings.ps1
-echo Set-NetAdapterAdvancedProperty -Name "Wi-Fi" -DisplayName "Preamble Mode" -DisplayValue "Short & long" >> settings.ps1
-echo Set-NetAdapterAdvancedProperty -Name "Wi-Fi" -DisplayName "Preferred Band" -DisplayValue "3. 5G first" >> settings.ps1
-echo Set-NetAdapterAdvancedProperty -Name "Wi-Fi" -DisplayName "Roaming Aggressiveness" -DisplayValue "1. Disable" >> settings.ps1
-echo Set-NetAdapterAdvancedProperty -Name "Wi-Fi" -DisplayName "Runtime D3" -DisplayValue "Disabled" >> settings.ps1
-echo Set-NetAdapterAdvancedProperty -Name "Wi-Fi" -DisplayName "TPCEnable" -DisplayValue "TPC Disable" >> settings.ps1
-echo Set-NetAdapterAdvancedProperty -Name "Wi-Fi" -DisplayName "Transmit Power" -DisplayValue "3. Medium" >> settings.ps1
-echo Set-NetAdapterAdvancedProperty -Name "Wi-Fi" -DisplayName "TxPowerControl" -DisplayValue "50%%" >> settings.ps1
-echo Set-NetAdapterAdvancedProperty -Name "Wi-Fi" -DisplayName "VHT 2.4G IOT" -DisplayValue "Disable" >> settings.ps1
-echo Set-NetAdapterAdvancedProperty -Name "Wi-Fi" -DisplayName "Wake on Magic Packet" -DisplayValue "Disabled" >> settings.ps1
-echo Set-NetAdapterAdvancedProperty -Name "Wi-Fi" -DisplayName "Wake on Pattern Match" -DisplayValue "Disabled" >> settings.ps1
-echo Set-NetAdapterAdvancedProperty -Name "Wi-Fi" -DisplayName "Wireless Mode" -DisplayValue "Auto" >> settings.ps1
+echo Set-NetAdapterAdvancedProperty -InterfaceDescription "RTK Killer Wi-Fi 5 8822CE Xtreme 802.11ac PCI-E" -DisplayName "11n Adhoc" -DisplayValue "Disabled" > settings.ps1
+echo Set-NetAdapterAdvancedProperty -InterfaceDescription "RTK Killer Wi-Fi 5 8822CE Xtreme 802.11ac PCI-E" -DisplayName "802.11d" -DisplayValue "Disabled" >> settings.ps1
+echo Set-NetAdapterAdvancedProperty -InterfaceDescription "RTK Killer Wi-Fi 5 8822CE Xtreme 802.11ac PCI-E" -DisplayName "802.11n channel width for 2.4GHz" -DisplayValue "Auto" >> settings.ps1
+echo Set-NetAdapterAdvancedProperty -InterfaceDescription "RTK Killer Wi-Fi 5 8822CE Xtreme 802.11ac PCI-E" -DisplayName "802.11n channel width for 5.2GHz" -DisplayValue "Auto" >> settings.ps1
+echo Set-NetAdapterAdvancedProperty -InterfaceDescription "RTK Killer Wi-Fi 5 8822CE Xtreme 802.11ac PCI-E" -DisplayName "Bandwidth" -DisplayValue "80+40+20Mhz" >> settings.ps1
+echo Set-NetAdapterAdvancedProperty -InterfaceDescription "RTK Killer Wi-Fi 5 8822CE Xtreme 802.11ac PCI-E" -DisplayName "Beacon Interval" -DisplayValue "100" >> settings.ps1
+echo Set-NetAdapterAdvancedProperty -InterfaceDescription "RTK Killer Wi-Fi 5 8822CE Xtreme 802.11ac PCI-E" -DisplayName "Classroom mode" -DisplayValue "Enabled" >> settings.ps1
+echo Set-NetAdapterAdvancedProperty -InterfaceDescription "RTK Killer Wi-Fi 5 8822CE Xtreme 802.11ac PCI-E" -DisplayName "Concurrent Operation Preference"-DisplayValue "No Preference" >> settings.ps1
+echo Set-NetAdapterAdvancedProperty -InterfaceDescription "RTK Killer Wi-Fi 5 8822CE Xtreme 802.11ac PCI-E" -DisplayName "HT mode" -DisplayValue "VHT mode" >> settings.ps1
+echo Set-NetAdapterAdvancedProperty -InterfaceDescription "RTK Killer Wi-Fi 5 8822CE Xtreme 802.11ac PCI-E" -DisplayName "MAC Randomization" -DisplayValue "Enabled" >> settings.ps1
+echo Set-NetAdapterAdvancedProperty -InterfaceDescription "RTK Killer Wi-Fi 5 8822CE Xtreme 802.11ac PCI-E" -DisplayName "Mixed mode protection" -DisplayValue "CTS-to-self Enabled" >> settings.ps1
+echo Set-NetAdapterAdvancedProperty -InterfaceDescription "RTK Killer Wi-Fi 5 8822CE Xtreme 802.11ac PCI-E" -DisplayName "Multi-Channel Concurrent" -DisplayValue "Enabled" >> settings.ps1
+echo Set-NetAdapterAdvancedProperty -InterfaceDescription "RTK Killer Wi-Fi 5 8822CE Xtreme 802.11ac PCI-E" -DisplayName "Preamble Mode" -DisplayValue "Short & long" >> settings.ps1
+echo Set-NetAdapterAdvancedProperty -InterfaceDescription "RTK Killer Wi-Fi 5 8822CE Xtreme 802.11ac PCI-E" -DisplayName "Preferred Band" -DisplayValue "3. 5G first" >> settings.ps1
+echo Set-NetAdapterAdvancedProperty -InterfaceDescription "RTK Killer Wi-Fi 5 8822CE Xtreme 802.11ac PCI-E" -DisplayName "Roaming Aggressiveness" -DisplayValue "1. Disable" >> settings.ps1
+echo Set-NetAdapterAdvancedProperty -InterfaceDescription "RTK Killer Wi-Fi 5 8822CE Xtreme 802.11ac PCI-E" -DisplayName "Runtime D3" -DisplayValue "Disabled" >> settings.ps1
+echo Set-NetAdapterAdvancedProperty -InterfaceDescription "RTK Killer Wi-Fi 5 8822CE Xtreme 802.11ac PCI-E" -DisplayName "TPCEnable" -DisplayValue "TPC Disable" >> settings.ps1
+echo Set-NetAdapterAdvancedProperty -InterfaceDescription "RTK Killer Wi-Fi 5 8822CE Xtreme 802.11ac PCI-E" -DisplayName "Transmit Power" -DisplayValue "3. Medium" >> settings.ps1
+echo Set-NetAdapterAdvancedProperty -InterfaceDescription "RTK Killer Wi-Fi 5 8822CE Xtreme 802.11ac PCI-E" -DisplayName "TxPowerControl" -DisplayValue "50%%" >> settings.ps1
+echo Set-NetAdapterAdvancedProperty -InterfaceDescription "RTK Killer Wi-Fi 5 8822CE Xtreme 802.11ac PCI-E" -DisplayName "VHT 2.4G IOT" -DisplayValue "Disable" >> settings.ps1
+echo Set-NetAdapterAdvancedProperty -InterfaceDescription "RTK Killer Wi-Fi 5 8822CE Xtreme 802.11ac PCI-E" -DisplayName "Wake on Magic Packet" -DisplayValue "Disabled" >> settings.ps1
+echo Set-NetAdapterAdvancedProperty -InterfaceDescription "RTK Killer Wi-Fi 5 8822CE Xtreme 802.11ac PCI-E" -DisplayName "Wake on Pattern Match" -DisplayValue "Disabled" >> settings.ps1
+echo Set-NetAdapterAdvancedProperty -InterfaceDescription "RTK Killer Wi-Fi 5 8822CE Xtreme 802.11ac PCI-E" -DisplayName "Wireless Mode" -DisplayValue "Auto" >> settings.ps1
 powershell -executionpolicy bypass -file settings.ps1
+echo pausing for 5secs...
+ping -n 5 localhost > nul
+del /q settings.ps1
 echo.
 echo.
 echo *********************************************************
@@ -56,13 +64,16 @@ echo *********************************************************
 echo.
 echo If there are a bunch of RED text error messages then something went wrong!
 echo 1. Make sure language / locale is set as English.
-echo 2. Make sure that the Wi-Fi interface name is set as Wi-Fi. 
-echo 3. Make sure that in Device Manager - Network Adapters it shows up as RTK Killer Wi-Fi.
-echo 4. Right-click setup.bat and run as administrator.
+echo 2. Right-click setup.bat and run as administrator.
 goto end
 
+:driver_error
+echo ****************************
+echo Wi-Fi driver install failed!
+echo ****************************
+echo Manually install the driver using Device Manager.
+
 :end
-del /q settings.ps1
 echo.
 pause
 exit
